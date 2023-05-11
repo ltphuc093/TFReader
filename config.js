0f9ce8da5cad52f229957eef7e4c5e70b2e57248
Le Phuc <phuclt093@gmail.com>
2023-05-11 10:24:51 +0700

    Chỉnh lại

--- config.js
+++ config.js
@@ -1,5 +1,14 @@
 var lauch_mode = "Development"; //Production or Development
 
 module.exports = {
-    lauch_mode: lauch_mode
+    lauch_mode: lauch_mode,
+    makers: [
+        {
+          name: '@electron-forge/maker-squirrel',
+          config: {
+            authors: 'Le Thanh Phuc',
+            description: 'Tool - Ứng đọc file ngôn ngữ Flutter'
+          },
+        },
+      ]
 }
